89049084d27034e77df036f9a496973c3fad2607
SydneyPurkapile <25995515-SydneyPurkapile@users.noreply.replit.com>
2024-01-18 01:01:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+print("Hello, world!")
+
+#This is a comment, HELLOOOOOOOO
+
+""" Math Stuff
++ combine
+- seperate
+* create a lot more than before
+/ divide into tiny piles
+// divide into tiny piles and throw away the extra
+% Mods
+** Pow Pow
+"""
+
+""" Assignments 
+= valuable
++= combine with
+-= remove from
+*= duplicate with
+/= split from
+//= self explainitory at this breakpoint
+%= you've figured it out
+**= 
+"""
+
+"""
+int = full thing
+float = decimal
+str = wordsssss
+chr = keyboard character
+bool = True or False
+"""
