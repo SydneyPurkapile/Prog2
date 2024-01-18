117e883ec3e161d20f3f9a58e8c119df4b3d7d01
SydneyPurkapile <143824003+SydneyPurkapile@users.noreply.github.com>
2024-01-18 01:12:34 +0000

    Update 1/17

--- prog52a.py
+++ prog52a.py
@@ -1,1 +1,8 @@
 # Area and Perimeter
+length = int(input("Enter Length: "))
+width = int(input("Enter Width: "))
+area = length * width
+perim = length * 2 + width * 2
+
+print("Area:", area)
+print("Perimeter:", perim)
