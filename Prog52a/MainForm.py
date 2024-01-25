71dcb06ff7c3676eeeeccedca761f8c805a53530
SydneyPurkapile <143824003+SydneyPurkapile@users.noreply.github.com>
2024-01-24 18:43:35 -0600

    Update 1/24

---
+++ Prog52a/MainForm.py
@@ -0,0 +1,183 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._button1 = System.Windows.Forms.Button()
+		self._label1 = System.Windows.Forms.Label()
+		self._textBox1 = System.Windows.Forms.TextBox()
+		self._label2 = System.Windows.Forms.Label()
+		self._textBox2 = System.Windows.Forms.TextBox()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self._label3 = System.Windows.Forms.Label()
+		self._label4 = System.Windows.Forms.Label()
+		self._label5 = System.Windows.Forms.Label()
+		self._label6 = System.Windows.Forms.Label()
+		self.SuspendLayout()
+		# 
+		# button1
+		# 
+		self._button1.BackColor = System.Drawing.SystemColors.ControlDark
+		self._button1.Font = System.Drawing.Font("Mongolian Baiti", 15.75, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(12, 172)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(110, 33)
+		self._button1.TabIndex = 0
+		self._button1.Text = "Calculate"
+		self._button1.UseVisualStyleBackColor = False
+		self._button1.Click += self.Button1Click
+		# 
+		# label1
+		# 
+		self._label1.BackColor = System.Drawing.SystemColors.ControlDark
+		self._label1.Font = System.Drawing.Font("Mongolian Baiti", 15.75, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.Location = System.Drawing.Point(12, 9)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(125, 23)
+		self._label1.TabIndex = 1
+		self._label1.Text = "Length:"
+		self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		# 
+		# textBox1
+		# 
+		self._textBox1.BackColor = System.Drawing.SystemColors.ControlDark
+		self._textBox1.Font = System.Drawing.Font("Mongolian Baiti", 14.25, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._textBox1.Location = System.Drawing.Point(143, 8)
+		self._textBox1.Name = "textBox1"
+		self._textBox1.Size = System.Drawing.Size(100, 29)
+		self._textBox1.TabIndex = 2
+		# 
+		# label2
+		# 
+		self._label2.BackColor = System.Drawing.SystemColors.ControlDark
+		self._label2.Font = System.Drawing.Font("Mongolian Baiti", 15.75, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._label2.Location = System.Drawing.Point(12, 41)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(125, 23)
+		self._label2.TabIndex = 3
+		self._label2.Text = "Width:"
+		self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		# 
+		# textBox2
+		# 
+		self._textBox2.BackColor = System.Drawing.SystemColors.ControlDark
+		self._textBox2.Font = System.Drawing.Font("Mongolian Baiti", 14.25, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._textBox2.Location = System.Drawing.Point(143, 40)
+		self._textBox2.Name = "textBox2"
+		self._textBox2.Size = System.Drawing.Size(100, 29)
+		self._textBox2.TabIndex = 4
+		# 
+		# button2
+		# 
+		self._button2.BackColor = System.Drawing.SystemColors.ControlDark
+		self._button2.Font = System.Drawing.Font("Mongolian Baiti", 15.75, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(136, 172)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(110, 33)
+		self._button2.TabIndex = 5
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = False
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.BackColor = System.Drawing.SystemColors.ControlDark
+		self._button3.Font = System.Drawing.Font("Mongolian Baiti", 15.75, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(76, 211)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(110, 33)
+		self._button3.TabIndex = 6
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
+		# 
+		# label3
+		# 
+		self._label3.BackColor = System.Drawing.SystemColors.ControlDark
+		self._label3.Font = System.Drawing.Font("Mongolian Baiti", 15.75, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._label3.Location = System.Drawing.Point(12, 97)
+		self._label3.Name = "label3"
+		self._label3.Size = System.Drawing.Size(125, 23)
+		self._label3.TabIndex = 7
+		self._label3.Text = "Area:"
+		self._label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		# 
+		# label4
+		# 
+		self._label4.BackColor = System.Drawing.SystemColors.ControlDark
+		self._label4.Font = System.Drawing.Font("Mongolian Baiti", 15.75, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._label4.Location = System.Drawing.Point(12, 129)
+		self._label4.Name = "label4"
+		self._label4.Size = System.Drawing.Size(125, 23)
+		self._label4.TabIndex = 8
+		self._label4.Text = "Perimeter:"
+		self._label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+		# 
+		# label5
+		# 
+		self._label5.BackColor = System.Drawing.SystemColors.ControlDark
+		self._label5.Font = System.Drawing.Font("Mongolian Baiti", 15.75, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._label5.Location = System.Drawing.Point(143, 97)
+		self._label5.Name = "label5"
+		self._label5.Size = System.Drawing.Size(100, 23)
+		self._label5.TabIndex = 9
+		self._label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+		# 
+		# label6
+		# 
+		self._label6.BackColor = System.Drawing.SystemColors.ControlDark
+		self._label6.Font = System.Drawing.Font("Mongolian Baiti", 15.75, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._label6.Location = System.Drawing.Point(143, 129)
+		self._label6.Name = "label6"
+		self._label6.Size = System.Drawing.Size(100, 23)
+		self._label6.TabIndex = 10
+		self._label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.SystemColors.ControlLight
+		self.ClientSize = System.Drawing.Size(258, 250)
+		self.Controls.Add(self._label6)
+		self.Controls.Add(self._label5)
+		self.Controls.Add(self._label4)
+		self.Controls.Add(self._label3)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._textBox2)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._textBox1)
+		self.Controls.Add(self._label1)
+		self.Controls.Add(self._button1)
+		self.Name = "MainForm"
+		self.Text = "Prog52a"
+		self.Load += self.MainFormLoad
+		self.ResumeLayout(False)
+		self.PerformLayout()
+
+
+	def Button1Click(self, sender, e):
+		L = int(self._textBox1.Text)
+		W = int(self._textBox2.Text)
+		A = L * W
+		P = L * 2 + W * 2
+		self._label5.Text = str(A)
+		self._label6.Text = str(P)
+
+	def Button2Click(self, sender, e):
+		self._label5.Text = ""
+		self._label6.Text = ""
+		self._textBox1.Text = ""
+		self._textBox2.Text = ""
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
+
+	def MainFormLoad(self, sender, e):
+		pass
