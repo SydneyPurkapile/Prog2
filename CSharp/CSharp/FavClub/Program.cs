475060ac5dc934c1d120565484a66c78c359fbc0
SydneyPurkapile <143824003+SydneyPurkapile@users.noreply.github.com>
2024-04-08 19:34:41 +0000

    Update 4/8

---
+++ CSharp/CSharp/FavClub/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavClub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your favorite sport: ");
+            string sport = Console.ReadLine();
+            Console.WriteLine("Your favorite sport is " + sport);
+
+            Console.ReadKey();
+        }
+    }
+}
